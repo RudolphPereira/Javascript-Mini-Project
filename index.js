d5b445c512962dd1da67edbce3a15b44941dbd28
Rudolph Pereira <rudolph.pereira93@gmail.com>
2020-06-26 01:26:01 +0530

    Added javascript code

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// let form = document.querySelector(".form");
-// let name = document.querySelector(".name");
-// let price = document.querySelector(".price");
-// let products = document.querySelector(".products");
+let form = document.querySelector(".form");
+let name = document.querySelector(".name");
+let price = document.querySelector(".price");
+let products = document.querySelector(".products");
 
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-//   let product = `
-//     <div class = 'product'
-//         <h1>${name.value}</h1>
-//         <p>${price.value}</p>
-//     </div>
-//   `;
+  let product = `
+    <div class = 'product'
+        <h1>${name.value}</h1>
+        <p>${price.value}</p>
+    </div>
+  `;
 
-//   products.insertAdjacentHTML("afterbegin", product);
-//   name.value = "";
-//   price.value = "";
-// });
+  products.insertAdjacentHTML("afterbegin", product);
+  name.value = "";
+  price.value = "";
+});
