afe77fb245d8b5cc8bdc47397f2cb7bf3df12626
Rudolph Pereira <rudolph.pereira93@gmail.com>
2020-06-26 01:22:07 +0530

    Upoaded Javascript mini project - 1st release

---
+++ index.js
@@ -0,0 +1,19 @@
+// let form = document.querySelector(".form");
+// let name = document.querySelector(".name");
+// let price = document.querySelector(".price");
+// let products = document.querySelector(".products");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   let product = `
+//     <div class = 'product'
+//         <h1>${name.value}</h1>
+//         <p>${price.value}</p>
+//     </div>
+//   `;
+
+//   products.insertAdjacentHTML("afterbegin", product);
+//   name.value = "";
+//   price.value = "";
+// });
